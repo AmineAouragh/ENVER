13fed760c0a96a3bc3c4a8118ae3b497c51ca624
AMINEAOURAGH <amine.aouragh.20@gmail.com>
2025-03-05 22:16:21 +0000

    commit

--- pages/tech-stack.js
+++ pages/tech-stack.js
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import Link from 'next/link' 
 import Image from 'next/image'
 import { useRouter } from 'next/router'
+import { useState } from 'react'
 import { DropDown } from '@/components/DropDown'
 import { FaAws } from "react-icons/fa"
 import { FaJava } from "react-icons/fa"
@@ -37,9 +38,45 @@ import { SiAwsamplify } from "react-icons/si"
 import { SiAwslambda } from "react-icons/si"
 import { SiAmazons3 } from "react-icons/si"
 import { SiAmazonrds } from "react-icons/si"
+import { FaCode } from "react-icons/fa"
+import { FaDatabase } from "react-icons/fa"
+import { GrTest } from "react-icons/gr"
+import { GoDatabase } from "react-icons/go"
+import { FiDatabase } from "react-icons/fi"
+import { TbSettingsBolt } from "react-icons/tb"
 
 
 export default function TechStack(){
+
+    const categories = [
+      { label: "Programming Languages", icon: <FaCode /> }, 
+      { label: "Frameworks and Libraries" }, 
+      { label: "Databases", icon: <FiDatabase /> }, 
+      { label: "DevOps and CI/CD", icon: <TbSettingsBolt /> }, 
+      { label: "Testing", icon: <GrTest /> }
+    ]
+
+    const programmingLanguages = [
+      { name: "java", label: "Java", icon: <FaJava /> },
+      { name: "python", label: "Python", icon: <FaPython /> },
+      { name: "javascript", label: "JavaScript", icon: <IoLogoJavascript /> }
+    ]
+    
+    const frameworks = [
+      { name: "react", label: "React", icon: <FaReact /> },
+      { name: "angular", label: "Angular", icon: <IoLogoAngular /> },
+      { name: "vue", label: "Vue", icon: <IoLogoVue /> }
+    ]
+
+    const databases = [
+      { name: "mysql", label: "MySQL", icon: <GrMysql /> },
+      { name: "postgresql", label: "PostgreSQL", icon: <BiLogoPostgresql /> },
+      { name: "mongodb", label: "MongoDB", icon: <BiLogoMongodb /> },
+      { name: "mariadb", label: "MariaDB", icon: <SiMariadb /> }
+    ]
+
+    const [ selectedCategory, setSelectedCategory ] = useState('')
+
     return (
         <>
           <Head>
@@ -60,8 +97,7 @@ export default function TechStack(){
             </nav>
             <section className='bg-slate-50 py-12 border border-gray-100 rounded-md my-20 w-full'>
                 <div className='w-3/4 mx-auto'>
-                  <h3 className='text-4xl font-BebasNeue mb-4'>Configure Your Tech Stack</h3>
-                  <p className='text-lg font-Inter text-gray-700'>Select and customize the technologies your team will use.</p>
+                  <h3 className='text-3xl font-Inter font-bold'>Configure Your Tech Stack</h3>
                 </div>
             </section>
             <section id="" className='w-full'>
@@ -69,14 +105,82 @@ export default function TechStack(){
                   <div className='w-1/4 py-4 px-3 bg-slate-50 border border-gray-100 rounded-md'>
                     <h4 className='text-md font-Inter font-semibold mb-6'>Technologies & Tools</h4>
                     <ul className='flex flex-col ml-6'>
-                        <li className='font-Inter text-lg mb-2'>Programming Languages</li>
-                        <li className='font-Inter text-lg mb-2'>Frameworks & Libraries</li>
-                        <li className='font-Inter text-lg mb-2'>Databases</li>
-                        <li className='font-Inter text-lg mb-2'>DevOps & CI/CD</li>
-                        <li className='font-Inter text-lg mb-2'>Testing</li>
+                      {
+                        categories.map(
+                          (category, i) => (
+                            <li key={i} tabIndex={0} onClick={() => setSelectedCategory(category.label)} className='font-Inter text-lg mb-2 flex flex-row items-center'>
+                              {category.icon && category.icon}
+                              <span className='ml-2'>{category.label}</span>
+                            </li>
+                          )
+                        )
+                      }
                     </ul>
                   </div>
-                  <div className='w-2/3 rounded-md p-4 bg-slate-50 border border-gray-100'></div>
+                  <div className='w-2/3 flex flex-col'>
+                    <div className='w-full rounded-md p-4 bg-slate-50 border border-gray-100 mb-6'>
+                      {
+                        selectedCategory.length > 0
+                        ?
+                          <div>
+                            <h4 className='text-2xl font-Inter font-bold mb-6'>{selectedCategory}</h4>
+                            {
+                              selectedCategory == "Programming Languages" && 
+                              <div className='flex flex-row items-center'>
+                                {
+                                  programmingLanguages.map(
+                                    (language, i) => (
+                                      <div key={i} tabIndex={0} className='rounded-md px-3 py-1 cursor-pointer bg-slate-100 border border-gray-200 text-xl mr-4 font-Inter flex flex-row items-center'>
+                                        {language.icon}
+                                        <span className='ml-2'>{language.label}</span>
+                                      </div>
+                                    )
+                                  )
+                                }
+                              </div>
+                            }
+                            {
+                              selectedCategory == "Frameworks and Libraries" && 
+                              <div className='flex flex-row items-center'>
+                                {
+                                  frameworks.map(
+                                    (framework, i) => (
+                                      <div key={i} tabIndex={0} className='rounded-md px-3 py-1 cursor-pointer bg-slate-100 border border-gray-200 text-xl mr-4 font-Inter flex flex-row items-center'>
+                                        {framework.icon}
+                                        <span className='ml-2'>{framework.label}</span>
+                                      </div>
+                                    )
+                                  )
+                                }
+                              </div>
+                            }
+                            {
+                              selectedCategory == "Databases" && 
+                              <div className='flex flex-row items-center'>
+                                {
+                                  databases.map(
+                                    (database, i) => (
+                                      <div key={i} tabIndex={0} className='rounded-md px-3 py-1 cursor-pointer bg-slate-100 border border-gray-200 text-xl mr-4 font-Inter flex flex-row items-center'>
+                                        {database.icon}
+                                        <span className='ml-2'>{database.label}</span>
+                                      </div>
+                                    )
+                                  )
+                                }
+                              </div>
+                            }
+                          </div>
+                        :
+                          <>
+                            <p className='font-bold text-xl font-Inter mb-4'>Technologies will show up here.</p> 
+                            <p className='font-Inter'>Try selecting a category in the left sidebar.</p>
+                          </>
+                      }
+                    </div>
+                    <div className='w-full rounded-md p-4 bg-slate-50 border border-gray-100'>
+
+                    </div>
+                  </div>
                 </div>
             </section>
           </div>
